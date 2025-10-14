d777ecd2c21117d4a5471fa24095416dacf0b355
Adriano Margarin <adriano.margarin@gmail.com>
2025-10-13 21:13:42 -0300

    feat: refatorado views

--- mentoria/blog/views.py
+++ mentoria/blog/views.py
@@ -8,16 +8,12 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
     ordering = ['-published_by']
-
-    def get_queryset(self):
-        return Post.objects.filter(published_at__isnull=False)
+    queryset = Post.objects.filter(published_at__isnull=False)
 
 
 class PostDetailView(DetailView):
     template_name = 'blog/post.html'
-
-    def get_queryset(self):
-        return Post.objects.filter(published_at__isnull=False)
+    queryset = Post.objects.filter(published_at__isnull=False)
 
     def get_object(self):
         return get_object_or_404(Post, category__slug=self.kwargs['category_slug'], slug=self.kwargs['post_slug'])
